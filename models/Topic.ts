9ee89572f86e23619ceac83b71d5df9e68f5f911
“sanyamharne” <harnesanyam@gmail.com>
2022-04-20 12:36:51 -0400

    Added Topic Model and Topic Schema

---
+++ models/Topic.ts
@@ -0,0 +1,12 @@
+/**
+ * @file Declares Topic data type representing details about a tuit
+ */
+
+ 
+ /**
+  * @typedef Tag Represents user definedd topics for a tuit
+  * @property {string} topic Text used by user to describe a tuit
+  */
+  export default interface Topic {
+   topic: string;
+}
