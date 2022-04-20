1362f0d93138474aeb7bf86034b9f1d0ee69865a
meet mehta <meetmehta10@gmail.com>
2022-04-20 12:58:41 -0400

    interfaces for bookmark screen

--- interfaces/BookMarkController.ts
+++ interfaces/BookMarkController.ts
@@ -2,9 +2,13 @@ import {Request, Response} from "express";
 /**
  * @file BookMarkController interface Represents web services for bookmark resources
  */
-export default interface BookMarkControllerI {
-    findAllUsersThatBookMarkedTuit(req: Request, res: Response): void;
-    findAllTuitsBookmarkedByUser(req: Request, res: Response): void;
-    userTogglesTuitBookMarks(req: Request, res: Response): void;
-    //userUnBookMarksTuit (req: Request, res: Response): void;
-};
+
+ export default interface BookmarkControllerI {
+     findAllUsersThatBookmarkedTuit (req: Request, res: Response): void;
+     findAllTuitsBookmarkedByUser (req: Request, res: Response): void;
+     userBookmarksTuit (req: Request, res: Response): void;
+     userUnbookmarksTuit (req: Request, res: Response): void;
+     userUnbookmarksAllTuit (req: Request, res: Response): void;
+     userTogglesTuitBookmarks (req: Request, res: Response): void;
+     findUserBookmarkedTuit (req: Request, res: Response): void;
+ };
--- interfaces/BookMarkDao.ts
+++ interfaces/BookMarkDao.ts
@@ -3,9 +3,10 @@ import BookMark from "../models/BookMark";
 /**
  * @file Declares API for BookMark related data access object methods
  */
-export default interface BookMarkI {
-    findAllUsersThatBookMarkedTuit (tid: string): Promise<BookMark[]>;
+ export default interface BookmarkDaoI {
+    findAllUsersThatBookmarkedTuit (tid: string): Promise<BookMark[]>;
     findAllTuitsBookmarkedByUser (uid: string): Promise<BookMark[]>;
-    userUnBookMarksTuit (tid: string, uid: string): Promise<any>;
-    userBookmarkedTuit (tid: string, uid: string): Promise<BookMark>;
+    userBookmarksTuit (tid: string, uid: string): Promise<BookMark>;
+    userUnbookmarksTuit (tid: string, uid: string): Promise<any>;
+    userUnbookmarksAllTuit (uid: string): Promise<any>;
 };
