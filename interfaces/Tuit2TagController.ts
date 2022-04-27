fcfee3a13acf4a0c95c1a4c6fb40d417895231ae
nandakishore2610 <kasula.n@northeastern.edu>
2022-04-27 10:30:07 -0400

    Added Tuit2Tag controller and Dao

---
+++ interfaces/Tuit2TagController.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from "express";
+
+export default interface Tuit2TagController {
+    createTuit2Tag(req: Request, res: Response): void;
+    deleteTuit2Tag(req: Request, res: Response): void;
+    findTagsByTuit(req: Request, res: Response): void;
+    findTuitsByTagText(req: Request, res: Response): void;
+    findAllTuit2Tags(req: Request, res: Response): void;
+    findTuit2TagsByTuit(req: Request, res: Response): void;
+};
